23a3f24b432d93ba535a019a9268138e3a094cbb
DongMyeongLee <dongmyeong.lee22@gmail.com>
2020-01-23 02:56:43 +0900

    기본 Q-Type 활용 및 검색 조건 쿼리 사용 및 함수이해(where 두가지 방법)

--- spring-querydsl/src/test/java/me/sun/springquerydsl/QuerydslBasicTest.java
+++ spring-querydsl/src/test/java/me/sun/springquerydsl/QuerydslBasicTest.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static me.sun.springquerydsl.entity.QMember.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -27,7 +28,7 @@ public class QuerydslBasicTest {
 
 
     @BeforeEach
-    public void init(){
+    public void init() {
 
         // 이렇게해도 동시성 문제가 없다.
         // em 자체가 멀티 스레드에 문제 없게 만들어져있다.
@@ -51,7 +52,7 @@ public class QuerydslBasicTest {
     }
 
     @Test
-    void startJPQL() throws Exception{
+    void startJPQL() throws Exception {
 
         Member findByJPQL = em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", "member1")
@@ -61,20 +62,25 @@ public class QuerydslBasicTest {
     }
 
     @Test
-    void startQueryDsl() throws Exception{
+    void startQueryDsl() throws Exception {
         //given
 
+        /* QueryDsl은 JPQL의 빌더 역할이므로 결국은 JPQL로 동작 된다.
+
+         */
         // 이미 만들어 진것을 사용할 것이다.
-        QMember m = new QMember("m");
+
+        //QMember m = new QMember("m"); 같은 테이블을 조인할 때는 이렇게 엘리어스를 다르게 해줘야 한다.
+        //QMember m = QMember.member;
 
         //when
 
         // jpql에서는 파라미터 바인딩을 안해준다
         // 이렇게 짜도 자동으로 프리페어 스테이트먼트의 파라미터 바인딩해서 넣어준다.
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         //then
@@ -82,6 +88,53 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
+    @Test
+    void search() throws Exception{
+        //given
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").and(member.age.eq(10)))
+                .fetchOne();
+        /*
+            eq -> =
+            ne -> !=
+            eq().net() -> !=
+
+            isNotNull()
+
+            in(10, 20)
+            notIn(10, 20)
+            between(10, 20)
+
+            goe(20) >=
+            gt >
+            loe <=
+            lt <
+
+            like(data%) -> data%
+            contains -> %data%
+            startsWith -> data%
+         */
+
+        //then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
 
+
+    @Test
+    void searchAndParam() throws Exception{
+        //given
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        // where and를 쉼표로 구분해도 된다.
+                        // null이 들어가면 null을 무시하므로 좀더 유용함
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                ).fetchOne();
+
+        //then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 }
